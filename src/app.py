c67f23e39cfb264ac3d3dd96cdf78ec9c05ee9ce
Aleksandar Babic <aleksandar.babic@transamerica.com>
2020-06-20 13:48:57 +0200

    Added swagger ui

--- src/app.py
+++ src/app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 from waitress import serve
+from flask_restplus import Api, Resource
+from clock_mirror import calculate_mirror_time, validate_mirror_time
 
 app = Flask(__name__)
+api = Api(app=app)
+clock_namespace = api.namespace('clock', description='Clock API')
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@clock_namespace.route("")
+class ClockHandler(Resource):
+    @api.doc(description='Endpoint that will return actual time for given mirror time.')
+    @api.doc(responses={500: 'Internal server error', 200: 'Time successfully converted',
+                        400: 'Bad request, no mirror_time given'},
+             params={'mirror_time': 'Time seen in the mirror'})
+    def get(self):
+        args = request.args
+        if 'mirror_time' not in args:
+            return "mirror_time is required", 400
+
+        try:
+            mirror_time = validate_mirror_time(args['mirror_time'])
+        except ValueError as e:
+            return str(e), 400
+
+        return {
+            'mirror_time': mirror_time,
+            'actual_time': calculate_mirror_time(mirror_time)
+        }
 
 
 if __name__ == '__main__':
