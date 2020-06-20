65dcb08552a184dbf72652584dcb0fdfc3783da4
Aleksandar Babic <aleksandar.babic@transamerica.com>
2020-06-20 15:58:04 +0200

    removed hardcoded cpu/memory for the ecs task def

--- .infra/app/ecs.tf
+++ .infra/app/ecs.tf
@@ -38,8 +38,8 @@ resource "aws_ecs_task_definition" "clockmirrorflask" {
   DEFINITION
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  memory                   = 512
-  cpu                      = 256
+  memory                   = var.ecs_task_memory
+  cpu                      = var.ecs_task_cpu
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 }
 
